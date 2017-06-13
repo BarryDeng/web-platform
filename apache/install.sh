bba4205708cabb8dfc6862ecf977d1ab808c3c70
BarryGates <1337762760@qq.com>
2017-06-14 00:23:16 +0800

    Fix apache

--- apache/install.sh
+++ apache/install.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get install -y php5-cli php5-cgi php5-fpm mysql-server
 rm /var/www/html/index.html
 
-cp ./bin/* /var/www/html/
+cp -r ./bin/* /var/www/html/
 mysql -e 'source /var/www/html/config.sql;'
 rm /var/www/html/config.sql
 
